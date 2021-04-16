7c4c9715a1badee5f8daac73dd3a33c9ae6c64f3
Eugene <clackd1amond@users.noreply.github.com>
2021-04-16 11:41:18 +0300

    spinner & error message on random block

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
+import Spinner from '../spinner';
+import ErrorMessage from '../errorMessage';
 export default class RandomChar extends Component {
 	constructor() {
 		super();
@@ -9,44 +11,53 @@ export default class RandomChar extends Component {
 	gotService = new gotService();
 	state = {
 		char: {},
+		loading: true,
+		error: false,
 	};
 
 	onCharLoaded = (char) => {
-		this.setState({ char });
+		this.setState({ char, loading: false });
+	};
+
+	onError = (err) => {
+		this.setState({ error: true, loading: false });
 	};
 
 	updateChar() {
 		const id = Math.floor(Math.random() * 140 + 25);
-		this.gotService.getCharacter(id).then(this.onCharLoaded);
+		this.gotService.getCharacter(id).then(this.onCharLoaded).catch(this.onError);
 	}
 
 	render() {
-		const {
-			char: { name, gender, born, died, culture },
-		} = this.state;
+		const { char, loading, error } = this.state;
+		const content = error ? <ErrorMessage /> : loading ? <Spinner /> : <View char={char} />;
 
-		return (
-			<div className='random-block rounded'>
-				<h4>Random Character: {name}</h4>
-				<ul className='list-group list-group-flush'>
-					<li className='list-group-item d-flex justify-content-between'>
-						<span className='term'>Gender </span>
-						<span>{gender}</span>
-					</li>
-					<li className='list-group-item d-flex justify-content-between'>
-						<span className='term'>Born </span>
-						<span>{born}</span>
-					</li>
-					<li className='list-group-item d-flex justify-content-between'>
-						<span className='term'>Died </span>
-						<span>{died}</span>
-					</li>
-					<li className='list-group-item d-flex justify-content-between'>
-						<span className='term'>Culture </span>
-						<span>{culture}</span>
-					</li>
-				</ul>
-			</div>
-		);
+		return <div className='random-block rounded'>{content}</div>;
 	}
 }
+
+const View = ({ char: { name, gender, born, died, culture } }) => {
+	return (
+		<>
+			<h4>Random Character: {name}</h4>
+			<ul className='list-group list-group-flush'>
+				<li className='list-group-item d-flex justify-content-between'>
+					<span className='term'>Gender </span>
+					<span>{gender}</span>
+				</li>
+				<li className='list-group-item d-flex justify-content-between'>
+					<span className='term'>Born </span>
+					<span>{born}</span>
+				</li>
+				<li className='list-group-item d-flex justify-content-between'>
+					<span className='term'>Died </span>
+					<span>{died}</span>
+				</li>
+				<li className='list-group-item d-flex justify-content-between'>
+					<span className='term'>Culture </span>
+					<span>{culture}</span>
+				</li>
+			</ul>
+		</>
+	);
+};
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './spinner.css';
+
+const Spinner = () => {
+	return (
+		<div class='loadingio-spinner-double-ring-u3j1uangsd'>
+			<div class='ldio-0w66qgrxey3'>
+				<div></div>
+				<div></div>
+				<div>
+					<div></div>
+				</div>
+				<div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Spinner;
