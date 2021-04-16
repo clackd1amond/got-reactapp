840d7a8a1f96006a0b6d133f447fd82e833fad39
Eugene <clackd1amond@users.noreply.github.com>
2021-04-16 14:05:59 +0300

    universal item list

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -4,11 +4,12 @@ import Spinner from '../spinner';
 import gotService from '../../services/gotService';
 export default class ItemList extends Component {
 	gotService = new gotService();
-	state = { charList: null };
+	state = { itemList: null };
 
 	componentDidMount() {
-		this.gotService.getAllCharacters().then((charList) => {
-			this.setState({ charList });
+		const { getData } = this.props;
+		getData().then((itemList) => {
+			this.setState({ itemList });
 		});
 	}
 
@@ -23,12 +24,13 @@ export default class ItemList extends Component {
 	}
 
 	render() {
-		const { charList } = this.state;
+		const { itemList } = this.state;
 
-		if (!charList) {
+		if (!itemList) {
 			return <Spinner />;
 		}
-		const items = this.renderItems(charList);
+
+		const items = this.renderItems(itemList);
 
 		return <ul className='item-list list-group'>{items}</ul>;
 	}
