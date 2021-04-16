35b1d7c31efc099e932226e3330e609a6eff049f
Eugene <clackd1amond@users.noreply.github.com>
2021-04-16 17:02:34 +0300

    404 handling

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,7 +5,7 @@ import Header from '../header';
 import RandomChar from '../randomChar';
 import ErrorMessage from '../errorMessage';
 import { CharacterPage, BookPage, HousePage, BooksItem } from '../pages';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 export default class App extends Component {
 	state = {
@@ -33,25 +33,46 @@ export default class App extends Component {
 						<Header />
 					</Container>
 					<Container>
-						<Row>
-							<Col lg={{ size: 5, offset: 0 }}>
-								<button className='btn btn-primary mb-4' onClick={this.toggleRandomCharBlock}>
-									Hide/show random block
-								</button>
-								{randomBlockRender}
-							</Col>
-						</Row>
-
-						<Route path='/characters' component={CharacterPage} />
-						<Route path='/houses' component={HousePage} />
-						<Route exact path='/books' component={BookPage} />
-						<Route
-							path='/books/:id'
-							render={({ match }) => {
-								const { id } = match.params;
-								return <BooksItem bookId={id} />;
-							}}
-						/>
+						<Switch>
+							<Route
+								path='/'
+								exact
+								render={() => {
+									return (
+										<Row>
+											<Col lg={{ size: 5, offset: 0 }}>
+												<button className='btn btn-primary mb-4' onClick={this.toggleRandomCharBlock}>
+													Hide/show random block
+												</button>
+												{randomBlockRender}
+											</Col>
+										</Row>
+									);
+								}}
+							/>
+							<Route path='/characters' component={CharacterPage} />
+							<Route path='/houses' component={HousePage} />
+							<Route exact path='/books' component={BookPage} />
+							<Route
+								path='/books/:id'
+								render={({ match }) => {
+									const { id } = match.params;
+									return <BooksItem bookId={id} />;
+								}}
+							/>
+							<Route
+								render={() => {
+									return (
+										<>
+											<h3 className='text-light'>Такой страницы не существует</h3>
+											<Link to='/' className='btn btn-primary btn-lg btn-block text-light'>
+												Вернуться на главную
+											</Link>
+										</>
+									);
+								}}
+							/>
+						</Switch>
 					</Container>
 				</div>
 			</Router>
