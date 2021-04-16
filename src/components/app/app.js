9326c7e0bcf4b1bc31b3f1f3031e1be5066fd1d9
Eugene <clackd1amond@users.noreply.github.com>
2021-04-16 11:52:13 +0300

    hide / show random block

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import {Col, Row, Container} from 'reactstrap';
+import React, { Component } from 'react';
+import { Col, Row, Container } from 'reactstrap';
 import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
 
+export default class App extends Component {
+	state = {
+		showRandomCharBlock: true,
+	};
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
-};
+	toggleRandomCharBlock = () => {
+		this.setState({ showRandomCharBlock: !this.state.showRandomCharBlock });
+	};
 
-export default App;
+	render() {
+		const randomBlockRender = this.state.showRandomCharBlock ? <RandomChar /> : null;
+		return (
+			<>
+				<Container>
+					<Header />
+				</Container>
+				<Container>
+					<Row>
+						<Col lg={{ size: 5, offset: 0 }}>
+							<button className='btn btn-primary mb-4' onClick={this.toggleRandomCharBlock}>
+								Hide/show random block
+							</button>
+							{randomBlockRender}
+						</Col>
+					</Row>
+					<Row>
+						<Col md='6'>
+							<ItemList />
+						</Col>
+						<Col md='6'>
+							<CharDetails />
+						</Col>
+					</Row>
+				</Container>
+			</>
+		);
+	}
+}
