5911cce4dc6bd1e0a10e94c47b430c2caf2cda30
Eugene <clackd1amond@users.noreply.github.com>
2021-04-16 12:21:48 +0300

    fix className in spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -3,8 +3,8 @@ import './spinner.css';
 
 const Spinner = () => {
 	return (
-		<div class='loadingio-spinner-double-ring-u3j1uangsd'>
-			<div class='ldio-0w66qgrxey3'>
+		<div className='loadingio-spinner-double-ring-u3j1uangsd'>
+			<div className='ldio-0w66qgrxey3'>
 				<div></div>
 				<div></div>
 				<div>
